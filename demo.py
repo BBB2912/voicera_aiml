0059a7f50a5202047821db4c966a05b9b0d3ed83
MAHENDRA KUMAR REDDY K <125733485+BBB2912@users.noreply.github.com>
2024-09-29 12:06:19 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -18,7 +18,7 @@ while cap.isOpened() and not stop_button_pressed:
     break
 
   frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-  frame_placeholder.image(frame,channels="RGB)
+  frame_placeholder.image(frame,channels="RGB")
 
   if cv2.waitKey(1) & 0xFF ==ord("q") or stop_button_pressed:
   break
